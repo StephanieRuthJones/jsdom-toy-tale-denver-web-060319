0d272fdc2d985baa36195b5d29e803d4bb491a8f
Stephanie Jones <stephanie.jones@flatiron.com>
2019-07-22 15:07:41 -0600

    Completed patch

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 const toyCollection = document.querySelector('.toy-collection')
+let toyName = document.querySelector('#toy-name')
+let toyImage = document.querySelector('#toy-image')
+const addToyButton = document.querySelector('#add-toy')
 let addToy = false
+
 
 // YOUR CODE HERE
 fetch('http://localhost:3000/toys')
@@ -10,6 +14,39 @@ fetch('http://localhost:3000/toys')
     return response.json();
   })
   .then(json => renderToys(json))
+
+let data = {
+  "name": `${toyName.innerHTML}`,
+  "image": `${toyImage.innerHTML}`,
+  "likes": 0
+}
+
+function postToy() {
+  fetch('http://localhost:3000/toys', {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.error('Error:', error));
+}
+
+addToyButton.addEventListener('click', function () {
+  event.preventDefault()
+  console.log("clicked")
+  fetch('http://localhost:3000/toys', {
+    method: 'POST', // or 'PUT'
+    body: JSON.stringify(data), // data can be `string` or {object}!
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(res => res.json())
+    .then(response => console.log('Success:', JSON.stringify(response)))
+    .catch(error => console.error('Error:', error));
+})
+
 
 
 function renderToys(json) {
@@ -33,13 +70,43 @@ function renderToys(json) {
     likes.innerHTML = `${toy.likes}`
     card.appendChild(likes)
 
-    const button = document.createElement('button')
-    button.innerHTML = 'Like'
-    button.className = 'like-btn'
-    card.appendChild(button)
+    const likeButton = document.createElement('button')
+    likeButton.innerHTML = 'Like'
+    likeButton.className = 'like-btn'
+    likeButton.id = `${toy.id}`
+    card.appendChild(likeButton)
+
+    likeButton.addEventListener('click', function () {
+      console.log("like button clicked")
+      currentLikeCount = likes.innerHTML
+      currentLikeCount++
+      console.log("new currentlikecount", currentLikeCount)
+      likeData = {
+        "likes": `${currentLikeCount}`
+      }
+      console.log("like data", likeData)
+      let id = event.target.id
+      console.log("id", id)
+      patchRequest(likeData, id)
+
+
+    })
+    ///attempt
+    function patchRequest(data, id) {
+      return fetch(`http://localhost:3000/toys/${id}`, {
+        credentials: 'same-origin', // 'include', default: 'omit'
+        method: 'PATCH', // 'GET', 'PUT', 'DELETE', etc.
+        body: JSON.stringify(data), // Coordinate the body type with 'Content-Type'
+        headers: new Headers({
+          'Content-Type': 'application/json'
+        }),
+      })
+        .then(response => response.json())
+    }
 
   })
 }
+
 
 
 addBtn.addEventListener('click', () => {
